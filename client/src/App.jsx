b01ae2728dd6a3c8cfed9e5d45e2ced575609390
kawamurasorachi <sorakawa0218@gmail.com>
2022-12-19 15:22:39 +0900

    chore: initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  )
+}
+
+export default App
