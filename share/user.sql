7897b70b290f635b5f026b1a67b47d03f9f63d6c
kawamurasorachi <sorakawa0218@gmail.com>
2022-12-19 17:49:58 +0900

    share: some sql

---
+++ share/user.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) PRIMARY KEY NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
