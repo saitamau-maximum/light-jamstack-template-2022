a23b481455baab3d7e5f276c075f064e2c821f7a
kawamurasorachi <sorakawa0218@gmail.com>
2022-12-19 18:18:49 +0900

    share: add insert sqls

---
+++ share/post_insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO `post` (`id`, `title`, `body`, `created_at`, `updated_at`, `user_id`) VALUES (1, 'Post 1', 'This is post 1', '2018-01-01 00:00:00', '2018-02-01 00:00:00', 1),
+       (2, 'Post 2', 'This is post 2', '2018-01-01 00:00:00', '2018-02-01 00:00:00', 2),
+       (3, 'Post 3', 'This is post 3', '2018-01-01 00:00:00', '2018-02-01 00:00:00', 3);
---
+++ share/user_insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `created_at`, `updated_at`) VALUES (1, 'user1', 'password1', 'hoge@example.com', '2018-01-01 00:00:00', '2018-02-01 00:00:00'),
+  (2, 'user2', 'password2', 'hoge@example.com', '2018-01-01 00:00:00', '2018-02-01 00:00:00'),
+  (3, 'user3', 'password3', 'hoge@example.com', '2018-01-01 00:00:00', '2018-02-01 00:00:00');
